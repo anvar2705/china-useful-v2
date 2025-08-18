fde0cc1d5321bacfe8752069249702dfd84c8c9c
anvar <anvar978@yandex.ru>
2025-08-18 13:23:41 +0400

    *wip

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const REPO_NAME = "china-useful-v2";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: "export",
+  basePath: isProd ? `/${REPO_NAME}` : "",
+  assetPrefix: isProd ? `/${REPO_NAME}` : "",
 };
 
 export default nextConfig;
